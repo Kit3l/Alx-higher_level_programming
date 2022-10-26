d884499288da9404ee3e97f6d363b93bcf618d10
Kit3l <pchumbakitel@gmail.com>
2022-10-25 23:58:52 -0700

    task 2final update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,4 +8,4 @@ appends to text file and returns num chars added
 def append_write(filename="", text=""):
     """appends to text file and returns num chars added"""
     with open(filename, mode="a", encoding="utf-8") as f:
-        return (f.write(text)
+        return (f.write(text))
