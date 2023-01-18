6662362dc9db4281dfa137c2bb1a61af00ca3bc9
Kit3l <pchumbakitel@gmail.com>
2023-01-17 22:27:01 -0800

    post json code update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a JSON POST request
-curl -sX POST -H 'Content-Type: application/json' -d "(cat "$2")" "$1"
+#sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s "{$1}" -X POST -H "Content-Type: application/json" -d @$2
