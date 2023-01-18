29d125521623a1bed0bfbc656dad60515c0ac0a4
Kit3l <pchumbakitel@gmail.com>
2023-01-17 21:50:21 -0800

    body 1 update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-if [ "$response" -eq 200 ]; then
-	curl -s "$1"
-else
-	echo "Error: Response code is not 200. Response code is $response"
-fi
+#takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL $1
