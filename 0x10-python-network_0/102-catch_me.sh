958663391f1d512b90c9791bdb4cc6ee103e6edc
Kit3l <pchumbakitel@gmail.com>
2023-01-18 02:41:33 -0800

    advanced task catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s "{$1}" -X POST -H "Content-Type: application/json" -d @$2
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
